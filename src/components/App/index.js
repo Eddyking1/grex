9933acc79685257fa4e79b69e9424d2899fe6755
Susanne Pettersson <susanne.pettersson@student.kyh.se>
2019-01-29 14:43:47 +0100

    User crud functionality in progress

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -11,22 +11,34 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
-
+import { withAuthentication } from '../Session';
 
 const App = () => (
-  <BrowserRouter>
-    <div>
-      <Navigation />
-      <hr/>
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </BrowserRouter>
+ <Router>
+   <div>
+     <Navigation />
+
+     <hr />
+
+     <Route exact path={ROUTES.LANDING} component={LandingPage} />
+     <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+     <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+     <Route
+       path={ROUTES.PASSWORD_FORGET}
+       component={PasswordForgetPage}
+     />
+     <Route path={ROUTES.HOME} component={HomePage} />
+     <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+     <Route path={ROUTES.ADMIN} component={AdminPage} />
+   </div>
+ </Router>
 );
 
-export default App;
+export default withAuthentication(App);
+
+
+
+
+
+
+
--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import AuthUserContext from './context';
+import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
 
-const Session = () => (
-  <div></div>
-);
+export { AuthUserContext, withAuthentication, withAuthorization }
 
-export default Session;
+
