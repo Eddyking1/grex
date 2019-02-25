c323bae798beb00a2fa5be13e29a9dfa972808f7
Eddyking1 <edward.kumerius@hotmail.com>
2019-02-25 13:23:30 +0100

    upload

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,57 +1,54 @@
-import React, { Component } from "react";
-import { withFirebase } from "../Firebase";
-import { withAuthorization } from "../Session";
-import styled from "styled-components";
-import { createGlobalStyle } from "styled-components";
+import React, { Component } from 'react';
+import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+
+
 
 const AdminStyle = styled.div`
   overflow: none;
-  display: flex;
-  justify-content: center;
-  text-aling: center;
-  padding: 20px;
+  display:flex;
+  justify-content:center;
+  text-aling:center;
+  padding:20px;
   background: hsl(225, 70%, 5%);
-  height: 100vh;
+  height:100vh;  
 `;
 
 const AdminText = styled.div`
-  display: flex;
-  width: 50vw;
-  padding: 10px;
+  display:flex;
+  width:50vw;
+  padding:10px;
   border: 2px solid;
-  flex-direction: column;
-
-  @media screen and (max-width: 1200px) {
-    width: 80vw;
-  }
-
-  @media screen and (max-width: 1000px) {
-    width: 80vw;
-  }
+  flex-direction:column;
 `;
 
 const Players = styled.div`
-  display: flex;
-  flex-direction: row;
+  display:flex;
+  flex-direction:row;
 
-  p {
-    display: flex;
-    justify-content: flex-end;
-    margin: 0 auto;
+  li {
+    display:flex;
+    justify-content:flex-end;
+    margin:0 auto;
   }
 `;
 
 const Head = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding-bottom: 10px;
-  
+  display:flex;
+  flex-direction:row;
+  padding-bottom:10px;
   h1 {
-    justify-content: center;
-    margin: 0 auto;
-    text-decoration: underline;
+    justify-content:center;
+    margin:0 auto;
+    text-decoration:underline;
   }
 `;
+
+
+
 
 class AdminPage extends Component {
   constructor(props) {
@@ -59,66 +56,77 @@ class AdminPage extends Component {
 
     this.state = {
       loading: false,
-      users: []
+      users: [],
     };
   }
+
+
+
 
   componentDidMount() {
     this.setState({ loading: true });
 
-    this.props.firebase.users().on("value", snapshot => {
+    this.props.firebase.users().on('value', snapshot => {
       const usersObject = snapshot.val();
 
       const usersList = Object.keys(usersObject).map(key => ({
         ...usersObject[key],
-        uid: key
+        uid: key,
       }));
+
+
 
       this.setState({
         users: usersList,
-        loading: false
+        loading: false,
       });
     });
   }
 
   componentWillUnmount() {
-  this.props.firebase.users().off();
+    this.props.firebase.users().off();
   }
 
   render() {
     const { users, loading } = this.state;
 
+
+
     return (
-      <div>
-        <h1>Admin</h1>
+      <AdminStyle>
         {loading && <div>Loading ...</div>}
         <UserList users={users} />
-      </div>
+      </AdminStyle>
     );
   }
 }
 
+
 const UserList = ({ users }) => (
   <div>
-    <Head>
+  <Head>
       <h1>Leaderboard</h1>
-    </Head>
-    <Players>
-      <p>Total players: {users.length}</p>
-      <p>Players online: {users.length}</p>
-    </Players>
+</Head>
+    <Players> 
+    <li>Total players: {users.length}</li>
+     <li>Players online: {users.length}</li>
+   </Players>
     {users.map(user => (
       <AdminText >
-        <p>
-          <strong>Username:</strong> {user.username}
-        </p>
-        <p>
-          <strong>Highscore:</strong> {user.highscore ? user.highscore : "0"}
-        </p>
+        <li key={user.uid}>
 
-        <p>
-          <strong>Distance traveled:</strong>{user.traveled ? user.traveled : "0"} <strong>Meters</strong>
-        </p>
+          <li>
+            <strong>Username:</strong> {user.username}
+          </li>
+          <li>
+            <strong>Highscore:</strong> {user.highscore ? user.highscore : "0" }
+          </li>
+
+          <li>
+            <strong>Distance traveled:</strong> {user.traveled ? user.traveled : "0"} <strong>Meters</strong>
+          </li>
+
+        </li>
       </AdminText>
     ))}
   </div>
